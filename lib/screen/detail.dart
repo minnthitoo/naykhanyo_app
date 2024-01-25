86933d31b4e12bb2b4bc5477a7266a119a976578
minnthitoo <mto.forlearning@gmail.com>
2024-01-25 17:30:03 +0630

    icon add

--- lib/screen/detail.dart
+++ lib/screen/detail.dart
@@ -15,7 +15,6 @@ class Detail extends StatefulWidget {
 }
 
 class _DetailState extends State<Detail> {
-
   final PoemController _poemController = Get.put(PoemController());
   @override
   void initState() {
@@ -23,63 +22,66 @@ class _DetailState extends State<Detail> {
     getDetail();
   }
 
-  getDetail() async{
+  getDetail() async {
     await _poemController.getPoem(widget.id);
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text(widget.title),
       ),
-      body: Obx(
-          (){
-            PoemState poemState = _poemController.poemState.value;
-            if(poemState is PoemLoadingState){
-              return Shimmer.fromColors(
-                  baseColor: Colors.grey,
-                  highlightColor: Colors.white,
-                child: Container(
-                  height: 20,
-                ),
-              );
-            }else if(poemState is PoemSuccessState){
-              return Shimmer.fromColors(
-                baseColor: Colors.grey,
-                highlightColor: Colors.white,
-                child: SingleChildScrollView(
-                  child: Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Container(
-                          height: 20,
-                          color: Colors.grey,
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Container(
-                          height: 300,
-                          color: Colors.grey,
-                        ),
-                      ],
+      body: Obx(() {
+        PoemState poemState = _poemController.poemState.value;
+        if (poemState is PoemLoadingState) {
+          return Shimmer.fromColors(
+            baseColor: Colors.grey,
+            highlightColor: Colors.white,
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 20,
+                      color: Colors.grey,
                     ),
-                  ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 20,
+                      color: Colors.grey,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 20,
+                      color: Colors.grey,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 300,
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                    ),
+                  ],
                 ),
-              );
-              // PoemModel poem = poemState.poem;
-              // return DetailWidget(poem: poem);
-            }else if(poemState is PoemErrorState){
-
-            }
-            return const Center();
-
-          }
-      ),
+              ),
+            ),
+          );
+        } else if (poemState is PoemSuccessState) {
+          PoemModel poem = poemState.poem;
+          return DetailWidget(poem: poem);
+        } else if (poemState is PoemErrorState) {}
+        return const Center();
+      }),
     );
   }
 }
